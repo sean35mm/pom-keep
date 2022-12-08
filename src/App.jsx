0034d8da92d18a3df1bae4d33acd7278c1cbc1d4
sean35mm <sean35mm@gmail.com>
2022-12-08 08:59:52 -0800

    cant get last break proper

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,101 @@
-import { useEffect, useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
-	const [sessions, setSessions] = useState(0);
-	const [breakSessions, setBreakSessions] = useState(0)
-	const [timer, setTimer] = useState(1 * 60);
-	const [shortBreakTime, setShortBreakTime] = useState(10 * 60)
-	const [longBreakTime, setLongBreakTime] = useState(25 * 60)
-	const [onBreak, setOnBreak] = useState(false);
-	const [timerActive, setTimerActive] = useState(false)
 
+	const [minutes, setMinutes] = useState(7);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
+  const [breakLength, setBreakLength] = useState(5);
+  const [pomodoroCount, setPomodoroCount] = useState(0);
 
-	useEffect(() => {
-	
-		if (timerActive === true) {
-			let interval = setInterval(() => {
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  const reset = () => {
+    setMinutes(7);
+    setSeconds(0);
+    setIsActive(false);
+    setIsBreak(false);
+    setBreakLength(5);
+    setPomodoroCount(0);
+  };
+
+  const startBreak = () => {
+    setIsBreak(true);
+    setMinutes(breakLength);
+    setSeconds(0);
+  };
+
+  const startPomodoro = () => {
+    setIsBreak(false);
+    setMinutes(7);
+    setSeconds(0);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
 				clearInterval(interval)
-				setTimer(timer - 1)
-				if (timer === 0){
-					setTimerActive(false)
-					setTimer(shortBreakTime)
-					setBreakSessions(breakSessions + 1)
-				}
-			}, 10)
-		}
-
-	
-		return () => {
-			
-		}
-	}, [timer, timerActive])
-	
-
-	const formatTime = (time) => {
-		let minutes = Math.floor(time / 60);
-		let seconds = time % 60;
-		return (
-			(minutes < 10 ? `0${minutes}` : minutes) +
-			':' +
-			(seconds < 10 ? `0${seconds}` : seconds)
-		);
-	};
-
-	const handleShortBreak = () => {
-		setTimerActive(false);
-		setOnBreak(true);
-		setTimer(shortBreakTime)
-	}
-
-	const handleLongBreak = () => {
-		setTimerActive(false);
-		setOnBreak(true);
-		setTimer(longBreakTime)
-	}
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0 && isActive === true) {
+            if (isBreak) {
+              startPomodoro();
+              setPomodoroCount(pomodoroCount + 1);
+            } else {
+              if (pomodoroCount === 4) {
+                setBreakLength(20)
 
 
+              }
+
+              startBreak();
+            }
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+
+      }, 1000);
+
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+
+  }, [isActive, seconds, minutes, isBreak, breakLength, pomodoroCount]);
 
 
-	
-
-	return (
-		<div className='bg-slate-400 flex flex-col items-center justify-center h-screen'>
-			<h1 className='text-2xl font-bold'>Pomodoro Keeper</h1>
-			<div>
-				<h3>Sessions Completed: {sessions}</h3>
-				<h3>Breaks Completed: {breakSessions}</h3>
-			</div>
-			<h1 className='text-9xl m-4'>{formatTime(timer)}</h1>
-			<div>
-				<button onClick={() => setTimerActive(!timerActive)} className='btn'>
-					{!timerActive ? "Start" : "Pause"}
-				</button>
-				<button className='btn'>
-					Reset
-				</button>
-			</div>
-			<h3 className='mt-4'>Want to set a break?</h3>
-				<div className="flex mt-4">
-					<button onClick={handleShortBreak} className='btn'>
-						Short Break
-					</button>
-					<button onClick={handleLongBreak} className='btn'>
-						Long Break
-					</button>
-				</div>
-		</div>
-	);
-}
-
+  return (
+    <div className='bg-slate-400 flex flex-col items-center justify-center h-screen'>
+			<h1>Pomodoro Timer</h1>
+			<h3>Session Count: {pomodoroCount}</h3>
+      <div className="text-4xl">
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </div>
+      <div className="row">
+        <button className={isActive ? `btn-secondary` : `btn`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+      
+    </div>
+  );
+};
 export default App;
+
+
+
---
+++ src/misc/docs.md
@@ -0,0 +1,7 @@
+# Pomodoro Lunar? Stellar? Space?
+
+45 minute intervals
+10 minute breaks
+25 minute long breaks
+
+time editable? 
